aaf31d954a1640147efcdfc6cdd6a9a8f6b3253c
bbaddoo <bbaddoo@luc.edu>
2025-07-14 14:52:07 +0000

    week 07 done

--- HashTable.java
+++ HashTable.java
@@ -37,7 +37,61 @@ public class HashTable<E extends Comparable<E>> {
      * @param content E The content of a new node, to be placed in the array.
      */
     public void add(E content) {
-        // COMPLETE THIS METHOD
-    } // method add
+        int hashCode = content.hashCode(); //retrive the hashcode
+          //make sure its not negative and within the array
+        int index = Math.abs(hashCode) % this.underlying.length;
 
-} // class HashTable
+        //create a new node with what is given previously
+        Node<E> newNode = new Node <E>(content);
+        //if the node is empty then put it here 
+        if (underlying[index] == null) {
+            underlying[index] = newNode;
+        } else {
+            // Make the new node point to the existing one
+            newNode.setNext(underlying[index]);
+
+            // put the new node at the front of the list
+            underlying[index] = newNode;
+        }
+        
+    } // method add
+/**
+     * returns a string representing the hash table 
+     * showing all the lists and whats in each one
+     * 
+     * @param Show full list.
+     */
+
+    public String toString() {
+        //start with an empty string 
+    String result = "";
+     //loop through each index of the array 
+    for (int i = 0; i < underlying.length; i++) {
+        //add the current one to the output
+        result += "Index " + i + ": ";
+         //get the first node at this index
+        Node<E> current = underlying[i];
+        //if there is no node then show that it is empty 
+        if (current == null) {
+            result += "empty";
+        } else { //if it is not empty then continue to add the other node to the output
+            while (current != null) {
+                result += current;
+                if (current.hasNext()) {
+                    result += " -> ";  //if theres another node add an arrow for simplicity
+                }
+                current = current.getNext();  //move to the next node 
+            }
+        }
+
+        result += "\n";  //move to a new line after each one to be cleaner 
+    }
+
+    return result;  //return the completed list in terms of the hash table.
+
+}
+}
+
+
+
+
